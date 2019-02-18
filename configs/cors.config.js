1d77bde8dd99efbb7ef8adfd27fb65ad430bdddb
Pablo Laso <plasocortabitarte@gmail.com>
2019-02-18 01:46:09 +0100

    config

--- configs/cors.config.js
+++ configs/cors.config.js
@@ -1,11 +1,8 @@
 const originsAllowed = [
   'https://gestor-corresponsales.herokuapp.com/',
-  'https://gestor-corresponsales.herokuapp.com',
   'http://gestor-corresponsales.herokuapp.com',
+  'https://app.corresponsalesdeguerra.com',
   'http://app.corresponsalesdeguerra.com',
-  'http://app.corresponsalesdeguerra.com/',
-  'http://localhost:3006',
-  'http://localhost:3000',
 ];
 
 module.exports = {
